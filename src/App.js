23d8b55334f7b28e2eef14d5c88e951dc21b1805
Sagar Gupta <112005736+sagargupta13@users.noreply.github.com>
2023-07-03 13:34:58 +0530

    Read data using Api

--- src/App.js
+++ src/App.js
@@ -1,15 +1,52 @@
 
+import { useEffect, useState } from 'react';
 import './App.css';
 import AxiosPost from './components/AxiosPost';
 import Create from './components/Create';
+import axios from 'axios';
 
 function App() {
+  const [columns,setCoulmns] =useState([])
+  const [records,setRecords] =useState([])
+  useEffect(()=>{
+    axios.get('  http://localhost:3030/users')
+    .then(res =>{
+      setCoulmns(Object.keys(res.data[0]))
+      setRecords(res.data)
+    })
+  },[])
   return (
-    <div className="container">
-     <Create/>
-     <AxiosPost/>
+    <div className="container mt-5">
+     {/* <Create/>
+     <AxiosPost/> */}
+<table className='table'>
+<thead>
+<tr>
+  {columns.map((c,i)=>(
+    <th key={i}> {c} </th>
+  ))}
+  <th>Action</th>
+  
+</tr>
+</thead>
+<tbody>
+  {
+    records.map((d,i)=>(
+      <tr key={i}>
+        <td>{d.id}</td>
+        <td>{d.name}</td>
+        <td>{d.email}</td>
+        <td>Up/De</td>
+      </tr>
+    ))
+    
+  }
+  
+</tbody>
+</table>
     </div>
+   
   );
-}
+}  
 
 export default App;
